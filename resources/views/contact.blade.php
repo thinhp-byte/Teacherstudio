9e6483a54837e271e4ea9d3a1f4e3af196134062
thinhp-byte <thinh.porrmann@code.berlin>
2025-09-14 18:05:56 +0700

    ADD nav bar across about and contact pages

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -21,6 +21,11 @@
         </style>
     </head>
     <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
-        <h1>contact us</h1>
+    <nav>
+            <a href="/">Home</a>
+            <a href="/about">About</a>
+            <a href="/contact">Contact</a>
+        </nav>    
+    <h1>contact us</h1>
     </body>
 </html>
