4a9129e9518117f0085b89bfd1f425e5907da0c6
thinhp-byte <thinh.porrmann@code.berlin>
2025-10-05 16:20:26 +0200

    REMOVED static data in resource model
    REMOVED static resource data and populated some fake data (manually) in Table plus

--- app/Models/resource.php
+++ app/Models/resource.php
@@ -1,39 +1,9 @@
 <?php
 
 namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
 
-class resource {
-    public static function all(): array
-{
-    return $resources =[
-            [
-                'id' => 1,
-                'title'=> 'mid term exam',
-                'subject' => 'math',
-                'grade' => '10',
-            ],
-            [
-                'id' => 2,
-                'title'=> 'final exam',
-                'subject' => 'biology',
-                'grade' => '11',
-            ],
-            [
-                'id' => 3,
-                'title'=> 'quiz 1',
-                'subject' => 'chemistry',
-                'grade' => '12',
-            ]
-            ];
-}
-
-public static function find (int $id): array
-{
-    $resource = Arr::first(static::all(), fn($resource) => $resource['id'] == $id);
-    if(!$resource) {
-        abort(404);
-    }
-    return $resource;
-}
+class resource extends Model{
+ 
 }
