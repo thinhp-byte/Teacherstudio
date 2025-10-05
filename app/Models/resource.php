40623b8ea26df075e53d71cce5660695b050f8d7
thinhp-byte <thinh.porrmann@code.berlin>
2025-10-05 17:46:29 +0200

    CREATED new Model Collection and FOREIGN ID in Resources
    Also used factory successfully to create fake data

--- app/Models/resource.php
+++ app/Models/resource.php
@@ -3,8 +3,10 @@
 namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class resource extends Model{
+    use HasFactory;
  
     protected $fillable = ['title', 'subject', 'grade'];
 }
--- database/migrations/2025_10_05_151316_create_collections_table.php
+++ database/migrations/2025_10_05_151316_create_collections_table.php
@@ -11,12 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('resources', function (Blueprint $table) {
+        Schema::create('collections', function (Blueprint $table) {
             $table->id();
-            $table->foreignIDfor(App\Models\Collection::class);
-            $table->string('title');
-            $table->string('subject');
-            $table->string('grade');
+            $table->string('name');
             $table->timestamps();
         });
     }
@@ -26,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('resources');
+        Schema::dropIfExists('collections');
     }
 };
