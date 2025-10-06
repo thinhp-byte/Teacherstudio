8956fb01101e81f14e8b0dfb736d43d2bd0cf210
thinhp-byte <thinh.porrmann@code.berlin>
2025-10-06 21:30:47 +0200

    CHANGED the design of the Resource Hub view

--- resources/views/resources.blade.php
+++ resources/views/resources.blade.php
@@ -1,12 +1,15 @@
 <x-layout>
+  <x-slot:heading>Resources</x-slot:heading>
   <h1>Resource Hub</h1>  
 
-  <ul>
+  <div class="mt-6 space-y-4">
    @foreach ($resources as $resource)
-    <li>
-      <a href="/resources/{{ $resource['id']}}">
-      <strong>{{ $resource['title']}}</strong> for {{$resource['subject']}} in {{$resource ['grade']}}th grade
-    </li>
+      <a href="/resources/{{ $resource['id']}}" class ="block px-4 py-6 border border-gray-200 rounded-lg">
+      <div class="font-bold text-blue-500 text-sm"->{{$resource->collection->name}}</div>
+      <div>
+          <strong>{{ $resource['title']}}</strong> for {{$resource['subject']}} in {{$resource ['grade']}}th grade
+      </div>      
+        </a>
   @endforeach
-</ul>
+</div>
 </x-layout>
