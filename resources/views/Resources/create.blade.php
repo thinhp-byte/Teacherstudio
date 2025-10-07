3495dfa6aa12c81273c0ab21971860b50da30e10
thinhp-byte <thinh.porrmann@code.berlin>
2025-10-08 01:34:31 +0200

    ADJUSTED Form Questionnaire

--- resources/views/Resources/create.blade.php
+++ resources/views/Resources/create.blade.php
@@ -6,28 +6,39 @@
    <form>
   <div class="space-y-12">
     <div class="border-b border-gray-900/10 pb-12">
-      <h2 class="text-base/7 font-semibold text-gray-900">Profile</h2>
-      <p class="mt-1 text-sm/6 text-gray-600">This information will be displayed publicly so be careful what you share.</p>
+      <h2 class="text-base/7 font-semibold text-gray-900">Create a New Resource</h2>
+      <p class="mt-1 text-sm/6 text-gray-600">Provide details here below</p>
 
       <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
         <div class="sm:col-span-4">
-          <label for="username" class="block text-sm/6 font-medium text-gray-900">Username</label>
+          <label for="title" class="block text-sm/6 font-medium text-gray-900">Title</label>
           <div class="mt-2">
             <div class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-              <div class="shrink-0 text-base text-gray-500 select-none sm:text-sm/6">workcation.com/</div>
-              <input id="username" type="text" name="username" placeholder="janesmith" class="block min-w-0 grow bg-white py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" />
+             
+              <input id="title" type="text" name="title" placeholder="Midterm Quiz" class="block min-w-0 grow bg-white py-1.5 px-3 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" />
             </div>
           </div>
         </div>
 
-        <div class="col-span-full">
-          <label for="about" class="block text-sm/6 font-medium text-gray-900">About</label>
+        <div class="sm:col-span-4">
+          <label for="subject" class="block text-sm/6 font-medium text-gray-900">Subject</label>
           <div class="mt-2">
-            <textarea id="about" name="about" rows="3" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"></textarea>
+            <div class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
+             
+              <input id="subject" type="text" name="subject" placeholder="Mathematics" class="block min-w-0 grow bg-white py-1.5 px-3 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" />
+            </div>
           </div>
-          <p class="mt-3 text-sm/6 text-gray-600">Write a few sentences about yourself.</p>
         </div>
 
+        <div class="sm:col-span-4">
+          <label for="grade" class="block text-sm/6 font-medium text-gray-900">Grade</label>
+          <div class="mt-2">
+            <div class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
+             
+              <input id="grade" type="text" name="grade" placeholder="5th" class="block min-w-0 grow bg-white py-1.5 px-3 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" />
+            </div>
+          </div>
+        </div>
         
 
        
