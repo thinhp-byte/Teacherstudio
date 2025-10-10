bda5c14c62734a07714f01d5a9ebbfbab14ddf56
thinhp-byte <thinh.porrmann@code.berlin>
2025-10-11 00:48:52 +0200

    BUG FIX
    typo in the routes of "resources" (mispelled "resource" instead) which lead to 404 error before

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'home')->name('home');
 Route::view('/contact', 'contact')->name('contact');
-Route::resource('resource', ResourceController::class);
+Route::resource('resources', ResourceController::class);
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
