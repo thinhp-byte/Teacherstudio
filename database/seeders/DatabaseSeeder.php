c40cc43c27bae583b059045261b97c048d0e785a
thinhp-byte <thinh.porrmann@code.berlin>
2025-10-12 20:30:11 +0200

    ADDED following each other logic in DatabaseSeeder and some sample follow between users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -53,5 +53,27 @@ class DatabaseSeeder extends Seeder
         $this->call([
             ResourceSeeder::class,
         ]);
+
+        // sample follows
+        $allUsers = User::all();
+        $admin = User::where('email', 'admin@admin.com')->first();
+        $testUser = User::where('email', 'test@example.com')->first();
+
+        // Admin follows 3 random users
+        $admin->following()->attach(
+            $allUsers->except([$admin->id])->random(3)->pluck('id')
+        );
+
+        // Test user follows admin and 2 others
+        $testUser->follow($admin);
+        $testUser->following()->attach(
+            $allUsers->except([$testUser->id, $admin->id])->random(2)->pluck('id')
+        );
+
+        // Random users follow admin (make admin popular)
+        $allUsers->except([$admin->id, $testUser->id])
+            ->random(5)
+            ->each(fn($user) => $user->follow($admin));
+
     }
 }
