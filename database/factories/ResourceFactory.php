9e236a110f653dbe748ccbb04310003430f21290
thinhp-byte <thinh.porrmann@code.berlin>
2025-10-12 20:02:11 +0200

    UPDATED ResourceFactory
    so resources are only created by users who have profiles.

--- database/factories/ResourceFactory.php
+++ database/factories/ResourceFactory.php
@@ -17,6 +17,17 @@ class ResourceFactory extends Factory
      */
     public function definition(): array
     {
+         $user = User::has('teacherProfile')->inRandomOrder()->first();
+        
+
+        $collection = Collection::firstOrCreate(
+            [
+                'user_id' => $user->id,
+                'name' => $user->name . "'s Collection"
+            ]
+        );
+
+        $subjects = ['Mathematics', 'English', 'Science', 'History', 'Art', 'Music', 'PE', 'Geography'];
         return [
             'title' => $this->faker->sentence(),
             'subject' => $this->faker->word(),
