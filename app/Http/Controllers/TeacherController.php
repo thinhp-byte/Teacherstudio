a2ae7e50ca2c52273b2efcb7fa839d3008781be4
thinhp-byte <thinh.porrmann@code.berlin>
2025-10-12 20:39:44 +0200

    CREATED TeacherController

---
+++ app/Http/Controllers/TeacherController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class TeacherController extends Controller
+{
+    public function index()
+    {
+        $teachers = User::has('teacherProfile')
+            ->with('teacherProfile')
+            ->withCount(['followers', 'collections'])
+            ->latest()
+            ->paginate(12);
+            
+        return view('teachers.index', [
+            'teachers' => $teachers
+        ]);
+    }
+}
